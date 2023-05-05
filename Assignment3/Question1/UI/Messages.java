94533ec785161dca57ada3d53a2b59d15903c2c1
Marcos Marquez <mmarquez144@toromail.csudh.edu>
2023-05-04 20:29:47 -0700

    type-o fix

--- Assignment3/Question1/UI/Messages.java
+++ Assignment3/Question1/UI/Messages.java
@@ -5,7 +5,7 @@ public class Messages
 	// Enter String
 	public static final String ENTER_STRING = "Enter the string you would like to see inverted.\n";
 	
-	// Ouptput
+	// Output
 	public static final String ORIGINAL_STRING  = "The original string is: \n%s";
 	public static final String INVERTED_STRING  = "The inverted string is: \n%s";
 }
